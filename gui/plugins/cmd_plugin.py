b6f519eab2420bcb48b91d760efdc2bf5741a0c2
Dusan Jakub <you@example.com>
2017-06-05 17:00:19 +0200

    Cmd plugin: Actualy use the supplied command

--- gui/plugins/cmd_plugin.py
+++ gui/plugins/cmd_plugin.py
@@ -1,62 +1,64 @@
 import shlex
 import subprocess
-from PyQt5 import QtGui
-from collections import OrderedDict
 from threading import Thread
 
-from PyQt5.QtCore import Qt, QSettings
+from PyQt5.QtCore import QSettings
 from PyQt5.QtCore import pyqtSignal
-from PyQt5.QtGui import QFont, QStandardItemModel, QStandardItem
-from PyQt5.QtWidgets import QPlainTextEdit, QDialog, QFormLayout, QLabel, QLineEdit, QCheckBox, QPushButton, \
-    QVBoxLayout, QHBoxLayout, QTreeView, QWidget
+from PyQt5.QtWidgets import QPlainTextEdit, QLineEdit, QPushButton, \
+    QVBoxLayout, QHBoxLayout, QWidget
 
-from gui.plugins.abstract_plugins import Plugin, GridPlugin, ContentViewPlugin, SettingsMenuPlugin, TabPlugin, \
+from gui.plugins.abstract_plugins import Plugin, TabPlugin, \
     SettingsPlugin
-from parser.http_parser import HttpMessage, HttpRequest
-from pipe.communication import RequestResponse
-from utils import soap2python
+
+DEFAULT_COMMAND = "while true; do echo \"" \
+                  "Proxy run on: ${local_address}:${local_port} -> ${remote_address}:${remote_port}\"; " \
+                  "sleep 1; done"
 
 
 class CmdPlugin(Plugin, SettingsPlugin, TabPlugin):
     def __init__(self):
         super().__init__("Command plugin")
-        self.command = "while true; do echo \"Proxy run on: ${local_address}:${local_port} -> ${remote_address}:${remote_port}\"; sleep 1; done"
+        self.widget = None
+        self.worker = None
 
     def save_settings(self, settings):
         settings.setValue("command_plugin_command", self.command)
 
     def restore_settings(self, settings: QSettings):
+
         if settings.value("command_plugin_command", None):
-            self.command = settings.value("command_plugin_command")
+            self.worker.command = settings.value("command_plugin_command")
 
     def get_tabs(self, data):
         yield lambda state: self.__build_tab(state), "Command runner"
 
     def __build_tab(self, state):
-        widget = QWidget()
-        vbox = QVBoxLayout()
+        if not self.widget:
+            self.worker = CmdWorder(self.plugin_registry.parameters)
+            self.worker.onOutput.connect(self.__on_output)
 
-        hbox = QHBoxLayout()
-        cmdEdit = QLineEdit()
-        cmdEdit.setText(self.command)
-        runBtn = QPushButton("Run / Stop")
-        runBtn.clicked.connect(self.__btnClicked)
+            self.widget = QWidget()
+            vbox = QVBoxLayout()
 
-        hbox.addWidget(cmdEdit)
-        hbox.addWidget(runBtn)
-        vbox.addLayout(hbox)
+            hbox = QHBoxLayout()
+            cmdEdit = QLineEdit()
+            cmdEdit.setText(self.worker.command)
+            cmdEdit.textChanged.connect(self.__commandChanged)
+            runBtn = QPushButton("Run / Stop")
+            runBtn.clicked.connect(self.__btnClicked)
 
-        self.output = QPlainTextEdit()
-        self.output.setReadOnly(True)
-        self.output.setPlainText("Output")
-        vbox.addWidget(self.output)
+            hbox.addWidget(cmdEdit)
+            hbox.addWidget(runBtn)
+            vbox.addLayout(hbox)
 
-        widget.setLayout(vbox)
+            self.output = QPlainTextEdit()
+            self.output.setReadOnly(True)
+            self.output.setPlainText("Output")
+            vbox.addWidget(self.output)
 
-        self.worker = CmdWorder(self.command, self.plugin_registry.parameters)
-        self.worker.onOutput.connect(self.__on_output)
+            self.widget.setLayout(vbox)
 
-        return widget
+        return self.widget
 
     def __btnClicked(self, event):
         if not self.worker.isRunning():
@@ -64,6 +66,9 @@ class CmdPlugin(Plugin, SettingsPlugin, TabPlugin):
             self.worker.start()
         else:
             self.worker.stop()
+
+    def __commandChanged(self, event):
+        self.worker.command = event
 
     def __on_output(self, outs_errs):
         outs, errs = outs_errs
@@ -83,11 +88,11 @@ class CmdPlugin(Plugin, SettingsPlugin, TabPlugin):
 class CmdWorder(QWidget):
     onOutput = pyqtSignal(tuple)
 
-    def __init__(self, command, parameters):
+    def __init__(self, parameters):
         super().__init__()
         self.parameters = parameters
         self.thread = None
-        self.command = command
+        self.command = DEFAULT_COMMAND
 
     def start(self):
         command = self.command
