7eabd010c0cab7304df0a292ba7d3db61d26136c
Dusan Jakub <timrodney@gmail.com>
2017-06-14 22:16:43 +0200

    Matcher for soap messages (basically dicts and lists)

--- proxy/pipe/recipe/soap.py
+++ proxy/pipe/recipe/soap.py
@@ -1,6 +1,7 @@
 from hamcrest.core.base_matcher import BaseMatcher
+from hamcrest.core.matcher import Matcher
 
-from proxy.pipe.recipe import Transform
+from proxy.pipe.recipe.transform import Transform
 
 
 class SoapTransform(Transform):
@@ -13,9 +14,53 @@ def soap_transform(client):
 
 
 class SoapMatches(BaseMatcher):
-    def __init__(self, soap_object, strict):
-        self.soap_object = soap_object
+    def __init__(self, pattern, strict):
+        self.pattern = pattern
         self.strict = strict
+
+    def _matches(self, item):
+        return SoapMatches.object_matches(self.pattern, item, self.strict)
+
+    @staticmethod
+    def object_matches(pattern, item, strict):
+        if isinstance(pattern, list):
+            return SoapMatches.list_matches(pattern, item, strict)
+        elif isinstance(pattern, dict):
+            return SoapMatches.dict_matches(pattern, item, strict)
+        elif isinstance(pattern, Matcher):
+            return pattern.matches(item)
+        elif callable(pattern):
+            return pattern(item)
+        else:
+            return pattern == item
+
+    @staticmethod
+    def list_matches(pattern, item, strict):
+        if len(pattern) > len(item) or (strict and len(pattern) != len(item)):
+            return False
+
+        item_index = 0
+        for pattern_index, pattern_value in enumerate(pattern):
+            while item_index < len(item):
+                item_value = item[item_index]
+                if SoapMatches.object_matches(pattern_value, item_value, strict):
+                    break
+                item_index += 1
+            else:  # no break
+                return False
+
+        return True
+
+    @staticmethod
+    def dict_matches(pattern, item, strict):
+        if len(pattern) > len(item) or (strict and len(pattern) != len(item)):
+            return False
+
+        for key, value in pattern.items():
+            if not SoapMatches.object_matches(value, item[key], strict):
+                return False
+
+        return True
 
 
 def soap_matches_loosely(soap_object):
---
+++ test/test_soap_matcher.py
@@ -0,0 +1,71 @@
+from hamcrest import greater_than
+
+from proxy.pipe.recipe.soap import SoapMatches
+
+
+def test_list_matcher():
+    assert SoapMatches.list_matches([], [], strict=True)
+    assert SoapMatches.list_matches([], [], strict=False)
+    assert not SoapMatches.list_matches([], [1], strict=True)
+    assert SoapMatches.list_matches([], [1], strict=False)
+
+    assert SoapMatches.list_matches([1, 2], [1, 2], strict=True)
+    assert SoapMatches.list_matches([1, 2], [1, 2], strict=False)
+
+    assert not SoapMatches.list_matches([1, 2], [1, 2, 3], strict=True)
+    assert SoapMatches.list_matches([1, 2], [1, 2, 3], strict=False)
+
+    assert not SoapMatches.list_matches([1, 3], [1, 2, 3], strict=True)
+    assert SoapMatches.list_matches([1, 3], [1, 2, 3], strict=False)
+
+    assert not SoapMatches.list_matches([2, 3], [1, 2, 3], strict=True)
+    assert SoapMatches.list_matches([2, 3], [1, 2, 3], strict=False)
+
+    assert not SoapMatches.list_matches([1, 4], [1, 2, 3], strict=True)
+    assert not SoapMatches.list_matches([1, 4], [1, 2, 3], strict=False)
+
+    assert not SoapMatches.list_matches([1, 2, 3, 4], [1, 2, 3], strict=True)
+    assert not SoapMatches.list_matches([1, 2, 3, 4], [1, 2, 3], strict=False)
+
+    assert not SoapMatches.list_matches([1, 1, 1], [1, 1, 1, 1], strict=True)
+    assert SoapMatches.list_matches([1, 1, 1], [1, 1, 1, 1], strict=False)
+    assert SoapMatches.list_matches([1, 1, 1, 2], [1, 1, 1, 1, 2], strict=False)
+
+
+def test_dict_matcher():
+    assert SoapMatches.dict_matches({}, {}, strict=True)
+    assert SoapMatches.dict_matches({}, {}, strict=False)
+    assert not SoapMatches.dict_matches({}, {"first": 1}, strict=True)
+    assert SoapMatches.dict_matches({}, {"first": 1}, strict=False)
+
+    assert SoapMatches.dict_matches({"first": 1, "second": 2}, {"first": 1, "second": 2}, strict=True)
+    assert SoapMatches.dict_matches({"first": 1, "second": 2}, {"first": 1, "second": 2}, strict=False)
+    assert SoapMatches.dict_matches({"second": 2, "first": 1}, {"first": 1, "second": 2}, strict=True)
+    assert not SoapMatches.dict_matches({"second": 22, "first": 1}, {"first": 1, "second": 2}, strict=False)
+
+    assert not SoapMatches.dict_matches({"first": 1, "second": 2}, {"first": 1, "second": 2, "third": 3}, strict=True)
+    assert SoapMatches.dict_matches({"first": 1, "second": 2}, {"first": 1, "second": 2, "third": 3}, strict=False)
+
+    assert not SoapMatches.dict_matches({"first": 1, "second": 2}, {"first": 1}, strict=True)
+    assert not SoapMatches.dict_matches({"first": 1, "second": 2}, {"first": 1}, strict=False)
+
+
+def test_combined_matchers():
+    assert SoapMatches.object_matches({"first": 1, "list": ['a', 'b']},
+                                      {"first": 1, "second": 2, 'list': ['a', 'b', 'c']}, strict=False)
+
+    assert SoapMatches.object_matches(lambda obj: len(obj) == 2,
+                                      {"first": 1, "list": ['a', 'b']},
+                                      strict=True)
+
+    assert not SoapMatches.object_matches({"first": 1, "list": lambda obj: len(obj) != 2},
+                                          {"first": 1, "list": ['a', 'b']},
+                                          strict=True)
+
+    assert not SoapMatches.object_matches({"first": greater_than(2)},
+                                          {"first": 1, "list": ['a', 'b']},
+                                          strict=False)
+
+    assert SoapMatches.object_matches({"first": greater_than(0)},
+                                      {"first": 1, "list": ['a', 'b']},
+                                      strict=False)
