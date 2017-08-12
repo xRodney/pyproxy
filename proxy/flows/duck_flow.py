dd8294af1b0015fd6675e1a4a21aef826ab8a429
Dusan Jakub <timrodney@gmail.com>
2017-08-13 01:09:28 +0200

    Flow definition generator is able to write whole classes

--- proxy/flows/duck_flow.py
+++ proxy/flows/duck_flow.py
@@ -8,6 +8,7 @@ from proxy.pipe.recipe.soap import soap_transform, SoapFlow
 
 def register_flow(flow: Flow):
     flow.delegate(DuckService().flow)
+    flow.delegate(DuckService3().flow)
     return flow
 
 
@@ -37,6 +38,25 @@ class DuckService:
         return self.flow.factory.duckAddResponse(
             result=115
         )
+
+    @flow.respond_soap(flow.factory.duckAdd())
+    def duck_add(self, request):
+        response = self.flow.factory.duckAddResponse(result=self.counter)
+        self.counter += 1
+        return response
+
+    @flow.respond
+    def else_response(self, request):
+        return HttpResponse(b"500",
+                            b"Unmatched request",
+                            b"The proxy is unable to mock the request")
+
+
+class DuckService3:
+    flow = SoapFlow(client, "/DuckService3")
+
+    def __init__(self):
+        self.counter = 15
 
     @flow.respond_soap(flow.factory.duckAdd())
     def duck_add(self, request):
--- proxygui/plugins/soap/SoapCodeGenerator.py
+++ proxygui/plugins/soap/SoapCodeGenerator.py
@@ -1,3 +1,4 @@
+import re
 from collections import OrderedDict
 
 from PyQt5.QtCore import pyqtSignal
@@ -22,39 +23,63 @@ class SoapCodeGenerator(QPlainTextEdit):
 
     def clear(self):
         self.setPlainText("")
-        self.__index_by_guid = OrderedDict()
-        self.__index_by_url = OrderedDict()
+        self.__index_by_path = OrderedDict()
 
     def refresh(self):
         pass
 
     def onLogChange(self, log, is_new, all_messages):
-        is_new = log.guid not in self.__index_by_guid
-
-        if self.soap_plugin.is_soap(log.response):
+        if log.request is not None and log.response is not None and self.soap_plugin.is_soap(log.response):
             snippet = self.soap_plugin.soap_code(log.response, log, level=1) + "\n"
 
-            if is_new:
-                start = self.__get_start_for_url(log.request)
-                old_length = 0
-            else:
-                start, old_length = self.__index_by_guid[log.guid]
-
-            self.__index_by_guid[log.guid] = (start, len(snippet))
+            start, old_length = self.__get_start_and_old_length(log, len(snippet))
             self.__replace_text(start, old_length, snippet)
 
-    def __get_start_for_url(self, request):
-        return len(self.toPlainText())
+    def __get_start_and_old_length(self, log, new_length):
+        path = log.request.path.decode()
+        guid = log.guid
+
+        if path in self.__index_by_path:
+            index = self.__index_by_path[path]
+            if guid in index:
+                # The log record already exists -> return cached values
+                old_length = index[guid][1]
+                start = index[guid][0]
+                index[guid] = start, new_length
+                return start, old_length
+            else:
+                # Log does not exists, but the definition does -> return position at the end of the class
+                last = next(reversed(index.values()))
+                start = last[0] + last[1]
+                index[guid] = start, new_length
+                return start, 0
+
+        else:
+            # Flow definition for path must be created
+            index = OrderedDict()
+            self.__index_by_path[path] = index
+
+            self.appendPlainText("class FlowDefinition{}:".format(re.sub('\W|^(?=\d)', '_', path)))
+            self.appendPlainText("    url = host + '{}?wsdl".format(path))
+            self.appendPlainText("    client = suds.client.Client(url)")
+            self.appendPlainText("    flow = SoapFlow(client, '{}')".format(path))
+            self.appendPlainText("\n")
+
+            start = len(self.toPlainText())
+            index[guid] = start, new_length
+            return start, 0
 
     def __replace_text(self, start, old_length, snippet):
         text = self.toPlainText()
         length = len(snippet)
+        old_end = start + old_length
 
-        text = text[:start] + snippet + text[start + old_length:]
+        text = text[:start] + snippet + text[old_end:]
         self.setPlainText(text)
 
         length_diff = length - old_length
         if length_diff != 0:
-            for index in self.__index_by_guid.values():
-                if index[0] > start:
-                    index[0] += length_diff
+            for index in self.__index_by_path.values():
+                for guid, record in index.items():
+                    if record[0] > old_end:
+                        index[guid] = record[0] + length_diff, length
